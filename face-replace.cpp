7ed6d5f52a060c9911253641f61f0c2460562ecb
Christopher Chin <ctchin13@gmail.com>
2016-07-28 21:05:43 -0400

    DEBUG output. Prepare for face rotation

--- face-replace.cpp
+++ face-replace.cpp
@@ -1,54 +1,54 @@
-// Reads a video file and outputs its frames to a new file
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv/cv.hpp>
 
 #include <string.h>
 #include <iostream>
+#include <math.h>
 
+#define DEBUG 0
+#define REQ_EYES 1
 typedef std::vector<std::pair<cv::Rect, std::vector<cv::Rect>>> face_data_t;
 
-// Removes all elements except the two largest regions
-std::vector<cv::Rect> largest_regions(std::vector<cv::Rect> &eyes)
+// Remove all regions except two largest. Currently uses height to determine
+// size
+void filter_largest2(std::vector<cv::Rect> &eyes)
 {
-    std::vector<cv::Rect> largest_regions;
-    if (eyes.size() < 2) {
-        return largest_regions; // Empty vector
-    } else if (eyes.size() > 2) {
-        // Remove all regions except two largest.
-        cv::Rect largest1 = eyes[0];
-        cv::Rect largest2 = eyes[1];
+    if (eyes.size() <= 2)
+        return;
 
-        // We'll just use height as size factor
-        if (largest2.height > largest1.height) {
-            cv::Rect tmp = largest2;
-            largest2 = largest1;
-            largest1 = tmp;
-        }
+    cv::Rect largest1 = eyes[0];
+    cv::Rect largest2 = eyes[1];
 
-        for (int j = 2; j < eyes.size(); j++) {
-            if (eyes[j].height > largest2.height) {
-                if (eyes[j].height > largest1.height) {
-                    cv::Rect tmp = largest1;
-                    largest1 = eyes[j];
-                    largest2 = tmp;
-                } else {
-                    largest2 = eyes[j];
-                }
+    if (largest2.height > largest1.height) {
+        cv::Rect tmp = largest2;
+        largest2 = largest1;
+        largest1 = tmp;
+    }
+
+    for (uint j = 2; j < eyes.size(); j++) {
+        if (eyes[j].height > largest2.height) {
+            if (eyes[j].height > largest1.height) {
+                cv::Rect tmp = largest1;
+                largest1 = eyes[j];
+                largest2 = tmp;
+            } else {
+                largest2 = eyes[j];
             }
         }
-        largest_regions.push_back(largest2);
-        largest_regions.push_back(largest1);
-        return largest_regions;
-    } else {
-        return eyes;
     }
+
+    // Remove all other elements in eyes array
+    eyes = std::vector<cv::Rect>();
+    eyes.push_back(largest1);
+    eyes.push_back(largest2);
 }
 
 // Returns an array of pairs. Face paired with eyes.
 #define CASCADE_PREFIX "/usr/share/opencv/haarcascades/"
 #define FACE_CASCADE "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
 #define EYE_CASCADE "/usr/share/opencv/haarcascades/haarcascade_eye.xml"
+#define DETECT_SCALE 1.1
 face_data_t face_detect(cv::Mat image) {
     // Convert image to gray
     cv::Mat gray_image;
@@ -60,24 +60,49 @@ face_data_t face_detect(cv::Mat image) {
 
     face_data_t face_data;
     std::vector<cv::Rect> faces;
-    face_cascade.detectMultiScale(gray_image, faces, 1.1, 2);
+    face_cascade.detectMultiScale(gray_image, faces, DETECT_SCALE, 2);
 
-    for (int i = 0; i < faces.size(); i++) {
+    for (uint i = 0; i < faces.size(); i++) {
         cv::Rect face_region = faces[i];
-        int x = face_region.x;
-        int y = face_region.y;
-        int w = face_region.width;
-        int h = face_region.height;
+
+        if (DEBUG) {
+            int x = face_region.x;
+            int y = face_region.y;
+            int w = face_region.width;
+            int h = face_region.height;
+            // Modulo is left-to-right, yes?
+            cv::rectangle(image, cv::Point(x, y), cv::Point(x + w, y + h), cv::Scalar(0, i * 5 % 255, 255), 2);
+        }
 
         // Generate a region of interest of face
         cv::Mat roi_gray(gray_image, face_region);
 
         // Detect eyes in face
         std::vector<cv::Rect> eyes;
-        eye_cascade.detectMultiScale(roi_gray, eyes, 1.1, 2);
+        eye_cascade.detectMultiScale(roi_gray, eyes, DETECT_SCALE, 2);
 
-        if (eyes.size() == 0)
+        if (DEBUG) {
+            for (uint j = 0; j < eyes.size(); j++) {
+                cv::Rect eye_region = eyes[j];
+                int x = face_region.x;
+                int y = face_region.y;
+                int ex = eye_region.x;
+                int ey = eye_region.y;
+                int ew = eye_region.width;
+                int eh = eye_region.height;
+
+                cv::Point point1(x + ex, y + ey);
+                cv::Point point2(x + ex + ew, y + ey + eh);
+
+                cv::rectangle(image, point1, point2, cv::Scalar(255, j * 5 % 255, 0), 2);
+            }
+        }
+
+        if (REQ_EYES && eyes.size() == 0)
             continue;
+
+        std::cout << eyes.size() << std::endl;
+        filter_largest2(eyes);
 
         std::pair<cv::Rect, std::vector<cv::Rect>> face_datum(face_region, eyes);
         face_data.push_back(face_datum);
@@ -101,6 +126,8 @@ void overlay_image(cv::Mat* src, cv::Mat* overlay, const cv::Point& location)
                 break;
 
             double opacity = ((double)overlay->data[fY * overlay->step + fX * overlay->channels() + 3]) / 255;
+            if (DEBUG)
+                opacity /= 2.0;
 
             for (int c = 0; opacity > 0 && c < src->channels(); c++) {
                 unsigned char overlayPx = overlay->data[fY * overlay->step + fX * overlay->channels() + c];
@@ -136,16 +163,45 @@ void crop_overlay(cv::Mat &image, cv::Mat &overlay, cv::Point location)
     overlay = overlay(cropped);
 }
 
+// Naively estimate head tilt and rotation based on eye location and size.
+// Assumes two eyes.
+// First value is head tilt. Second is head turn.
+std::pair<int, int> estimate_angle(std::vector<cv::Rect> &eyes) {
+    cv::Rect eye0 = eyes[0];
+    cv::Rect eye1 = eyes[1];
+
+    int height_delta = eye0.y - eye1.y;
+    int width_delta = eye0.x - eye1.x;
+    double angle = atan((double)height_delta / width_delta);
+
+    printf("%d %d %f\n", height_delta, width_delta, angle);
+
+    return std::pair<int, int>(0, 0);
+}
+
 #define SCALE 1.1
 #define OFFSET 0.15
 void face_replace(cv::Mat image, cv::Mat replacement, face_data_t &face_data)
 {
-    for (int i = 0; i < face_data.size(); i++) {
+    for (uint i = 0; i < face_data.size(); i++) {
         cv::Rect face_region = face_data[i].first;
         int x = face_region.x;
         int y = face_region.y;
         int w = face_region.width;
         int h = face_region.height;
+
+        // Attempt face rotation
+        /*
+        if (face_data[i].second.size() == 2) {
+            std::pair<double, double> head_angles = estimate_angle(face_data[i].second);
+            double head_tilt = head_angles.first;
+            cv::Point2f re_center(replacement.cols/2.0F, replacement.rows/2.0F);
+            cv::Mat rot_mat = getRotationMatrix2D(re_center, 90, 1.0);
+            cv::Mat dst;
+            warpAffine(replacement, dst, rot_mat, replacement.size());
+            replacement = dst;
+        }
+        */
 
         // Resize replacement to have same height as image
         // Modify by SCALE
